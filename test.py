cb621ef91efae60b7fef48ea95eabeeffe71d624
Viktor Likin <viktor.likin@gmail.com>
2014-09-27 14:32:57 +0300

    Test database is ignored.

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ import unittest
 
 #from ext.budget.test.user_model import UserModelTestCase
 from ext.budget.test.budget_model import BudgetModelTestCase
-#from ext.budget.test.tag_model import TagModelTestCase
+from ext.budget.test.tag_model import TagModelTestCase
+from ext.budget.test.expense_model import ExpenseModelTestCase
 #from ext.budget.test.table import TableTestCase
 
 if __name__ == '__main__':
