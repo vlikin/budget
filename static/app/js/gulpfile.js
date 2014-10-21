b6e00ba0f5c6d310812a424191d90d05bb14df29
Viktor Likin <viktor.likin@gmail.com>
2014-10-21 13:19:29 +0300

    Third party libraries have been compiled.

--- static/app/js/gulpfile.js
+++ static/app/js/gulpfile.js
@@ -17,6 +17,42 @@ gulp.task('compile_app', function () {
     .pipe(gulp.dest('.'));
 });
 
+gulp.task('compile_3pjs', function () {
+  gulp.src([
+      '../bower_components/angular/angular.js',
+      '../bower_components/angular-animate/angular-animate.js',
+      '../bower_components/angular-loading-bar/build/loading-bar.js',
+      '../bower_components/angular-local-storage/angular-local-storage.js',
+      '../bower_components/angular-message-center/dist/message-center.js',
+      '../bower_components/angular-mocks/angular-mocks.js',
+      '../bower_components/angular-route/angular-route.js',
+      '../bower_components/jquery/jquery.js',
+      '../bower_components/bootstrap/dist/js/bootstrap.js'
+    ])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(concat('3p.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('compile_3pcss', function () {
+  gulp.src([
+      '../bower_components/bootstrap/dist/css/bootstrap.css',
+      '../bower_components/bootstrap/dist/css/bootstrap-theme.css',
+      '../bower_components/angular-message-center/dist/message-center.css',
+      '../bower_components/angular-loading-bar/build/loading-bar.css'
+    ])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(concat('3p.css'))
+    //.pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('.'));
+});
+
 gulp.task('watch', function () {
   watch('src/**/*.js', function (files, cb) {
     gulp.start('compile_app', cb);
