2a32ebbb5d4d949ae2b8814357b9bcc92832a13f
Viktor Likin <viktor.likin@gmail.com>
2014-11-13 19:11:47 +0200

    Test data for tests has been extended.

--- ext/shell/test_data.py
+++ ext/shell/test_data.py
@@ -1,5 +1,5 @@
 test_data = dict(
-    users = ['user_1', 'user_2', 'user_3'],
+    users = ['user_1', 'user_2', 'user_3', 'user_4', 'user_5', 'user_6'],
     user_objects = {},
     budgets = [
       dict(
@@ -38,6 +38,45 @@ test_data = dict(
             amount='160',
             tags = ['tag 2', 'tag 3'],
             description = 'user_2 income.'
+          ),
+        ],
+      ),
+      dict(
+        title = 'budget 2',
+        budget_object = None,
+        users = ['user_2', 'user_3', 'user_4'],
+        tags = ['tag 1', 'tag 2', 'tag 3', 'tag 4'],
+        tag_objects = dict(),
+        contributions = [
+          dict(
+            user='user_2',
+            amount='200'
+          ),
+          dict(
+            user='user_2',
+            amount='400'
+          ),
+          dict(
+            user='user_3',
+            amount='600'
+          ),
+          dict(
+            user='user_4',
+            amount='800'
+          ),
+        ],
+        expenses = [
+          dict(
+            user='user_4',
+            amount='90',
+            tags = ['tag 1', 'tag 4'],
+            description = 'user_4 expense.'
+          ),
+          dict(
+            user='user_3',
+            amount='110',
+            tags = ['tag 2', 'tag 3'],
+            description = 'user_4 expense.'
           ),
         ],
       )
