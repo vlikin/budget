fdb751ad9b5c5a24d10b3e3b8c008e84502e7760
Viktor Likin <viktor.likin@gmail.com>
2014-11-25 17:36:04 +0200

    Expense calculation moved to a function.

--- ext/shell/real_data_v1/lib.py
+++ ext/shell/real_data_v1/lib.py
@@ -17,6 +17,8 @@ def init_real_data_v1():
     for tag_name, tag in tags.items():
       parent_id = parent['obj']['id'] if parent else None
       flat_tags[tag_name] = tag
+      flat_tags[tag_name]['expenses'] = list()
+      flat_tags[tag_name]['volume'] = 0
       if '_' in tag:
         go_over(tag['_'], parent)
 
@@ -25,15 +27,19 @@ def init_real_data_v1():
   with open(os.path.join(data_dir, 'expenses.csv')) as csv_file:
     reader = csv.reader(csv_file)
     for row in reader:
-      tag_list = row[2]
-      # @todo - pass expenses expenses.
-      expenses.append([row[0], row[1], row[2]])
+      tag_list = [ s.strip() for s in row[2].split(',') ]
+      expense = [row[0], row[1], tag_list]
+      expenses.append(expense)
+      for tag in tag_list: 
+        flat_tags[tag]['volume'] = flat_tags[tag]['volume'] + float(expense[1].replace(',', '.')) 
+        flat_tags[tag]['expenses'].append(expense)
 
   return dict(
     tags=tags,
     flat_tags=flat_tags,
     expenses=expenses
   )
+
 
 def display_tree(tags):
   '''
