48749e5b3ab745c5533ee132d819d45b6acb3da2
Viktor Likin <viktor.likin@gmail.com>
2014-12-08 19:54:45 +0200

    The new version of lib has been modified.

--- ext/shell/real_data_v1/lib/lib.py
+++ ext/shell/real_data_v1/lib/lib.py
@@ -1,38 +1,75 @@
+from copy import deepcopy
+
 import json
+import pdb
 
 def load_tags(path):
-
-  # Tag creation.
+  '''
+    - It loads tags from a JSON file.
+  '''
   with open(path) as json_file:
     tags = json.load(json_file)
 
+  return tags
 
-  def back_link_callback(tag_kv, parent_kv, scope=None):
-    print '---> 11'
-    tag_name, tag = tag_kv
-    parent_name, parent = parent_kv
-    tag['parent'] = parent_kv
+def get_flat_tags(tags):
+  '''
+    - It Moves a tree hierarchy into a flat list.
+  '''
+  scope = dict(
+    flat_tags=dict()
+  )
+  __go_over(__get_flat_tags_callback, tags, (None, None), scope)
 
-  go_over(back_link_callback, tags)
+  return scope['flat_tags']
 
-  def calculate_level_callback(tag_kv, parent_kv, scope):
-    tag_name, tag = tag_kv
-    parent_name, parent = parent_kv
-    if not tag['parent']:
-      tag['level'] = 0
-    else:
-      if 'level' not in tag['parent'][1]:
-        tag['parent'][1]['level'] = 0
-      tag['level'] = tag['parent'][1]['level'] + 1
+def prepare_tags(loaded_tags):
+  '''
+    - It prepares the loaded tags.
 
-  go_over(calculate_level_callback, tags)
+    It copies the loaded tags and attaches additional
+    data that helps for calculations.
+  '''
+  tags = deepcopy(loaded_tags)
+  __go_over(__back_link_callback, tags)
+  __go_over(__calculate_level_callback, tags)
 
   return tags
 
-
-def go_over(callback, tags, parent_kv=(None, None), scope=None):
+def __go_over(callback, tags, parent_kv=(None, None), scope=None):
+  '''
+    - It goes through the tags and call the callback.
+  '''
   parent_tag_name, parent_tag = parent_kv
   for tag_name, tag in tags.items():
     callback((tag_name, tag), parent_kv, scope)
     if '_' in tag:
-      go_over(callback, tag['_'], (tag_name, tag), scope)
+      __go_over(callback, tag['_'], (tag_name, tag), scope)
+
+def __get_flat_tags_callback(tag_kv, parent_kv, scope):
+  '''
+    - The callback that set a level for the current tag.
+  '''
+  tag_name, tag = tag_kv
+  scope['flat_tags'][tag_name] = tag
+
+def __calculate_level_callback(tag_kv, parent_kv, scope):
+  '''
+    - The callback that set a level for the current tag.
+  '''
+  tag_name, tag = tag_kv
+  parent_name, parent = parent_kv
+  if not tag['parent'][0]:
+    tag['level'] = 0
+  else:
+    if 'level' not in tag['parent'][1]:
+      tag['parent'][1]['level'] = 0
+    tag['level'] = tag['parent'][1]['level'] + 1
+
+def __back_link_callback(tag_kv, parent_kv, scope=None):
+  '''
+    - The callback that set a level for the current tag.
+  '''
+  tag_name, tag = tag_kv
+  parent_name, parent = parent_kv
+  tag['parent'] = parent_kv
--- ext/shell/real_data_v1/lib/test_lib.py
+++ ext/shell/real_data_v1/lib/test_lib.py
@@ -1,6 +1,8 @@
+
 from unittest import TestCase
 import os
-from lib import load_tags
+import pdb
+from lib import load_tags, prepare_tags, get_flat_tags
 
 class LibCase(TestCase):
   @classmethod
@@ -14,10 +16,18 @@ class LibCase(TestCase):
     pass
 
   def test_lib(self):
-    print 'Hi!'
     data_folder = os.path.join(os.path.dirname(__file__), 'data_test')
     tags_path = os.path.join(data_folder, 'tags.json')
-    tags = load_tags(tags_path)
-    print data_folder
-    print tags
+    
+    loaded_tags = load_tags(tags_path)
+    assert loaded_tags['Tag_2']['_']['Tag_2_3']['_']['Tag_2_3_2'] != None
+    
+    tags = prepare_tags(loaded_tags)
+    assert tags['Tag_2']['_']['Tag_2_3']['_']['Tag_2_3_2']['parent'][0] == 'Tag_2_3'
+    assert tags['Tag_2']['_']['Tag_2_3']['_']['Tag_2_3_2']['level'] == 2
+
+    flat_tags = get_flat_tags(tags)
+    assert flat_tags.has_key('Tag_1')
+    assert flat_tags.has_key('Tag_2_3_2')
+
     assert 1 == 1
