a5d38d9cc49d5cd11bf78ee9b8b2bced8d9553a2
Viktor Likin <viktor.likin@gmail.com>
2014-12-10 18:21:28 +0200

    Capacity calculation has been fixed.

--- ext/shell/real_data_v1/lib/lib.py
+++ ext/shell/real_data_v1/lib/lib.py
@@ -1,7 +1,98 @@
 from copy import deepcopy
+from datetime import datetime
+from IPython.display import HTML
 
+import csv
 import json
 import pdb
+
+def display_tree(tags):
+  '''
+    - It renders HTML tree view of tags.
+  '''
+  if type(tags) is dict and tags.has_key('_'):
+    tags = tags['_']
+
+  def go_over(tags, parent=None):
+    '''
+      - It goes over a tag tree.
+    '''
+    html = ''
+    for tag_name, tag in tags.items():
+      html_item = '<li><span>%s</span>' % tag_name
+      if '_' in tag:
+        html_inside = go_over(tag['_'], tag)
+        html_item = '%s<ul>%s</ul>' % (html_item, html_inside)
+      html_item = '%s</li>' % html_item
+      html = '%s%s' % (html, html_item)
+
+    return html
+
+  return HTML(go_over(tags))
+
+
+def calculate_tag_capacity(tag_name, tag):
+  '''
+    - It calculates a capacity of the tag, volumes of all inside tags.
+  '''
+  if 'volume' not in tag:
+    tag['volume'] = 0
+    tag['expenses'] = list()
+
+  capacity = tag['volume']
+  if '_' in tag:
+    for inside_tag_name, inside_tag in tag['_'].iteritems():
+      if '_' not in tag or len(tag['_']) == 0:
+        capacity = capacity + inside_tag['volume']
+      else:
+        capacity = capacity + calculate_tag_capacity(inside_tag_name, inside_tag)
+  tag['capacity'] = capacity
+
+  return capacity
+
+def calculate_tags_capacity(tags):
+  '''
+    - It calculates capacities of all tags.
+  '''
+  for tag_name, tag in tags.iteritems():
+    calculate_tag_capacity(tag_name, tag)
+
+def attach_expenses2tags(expenses, tags):
+  '''
+    - It attaches expenses to tags, it sets a tag volume.
+  '''
+  if type(tags) is dict:
+    flat_tags = get_flat_tags(tags)
+  elif type(tags) is list:
+    flat_tags = tags
+
+  for expense in expenses:
+    for tag_name in expense[2]:
+      tag = flat_tags[tag_name]
+      if 'expenses' not in tag:
+        tag['expenses'] = list()
+      if 'volume' not in tag:
+        tag['volume'] = 0
+
+      tag['expenses'].append(expense)
+      tag['volume'] = tag['volume'] + expense[1]
+
+
+def load_expenses(path):
+  '''
+    - It loads expenses from a file.
+  '''
+  expenses = []
+  with open(path) as csv_file:
+    reader = csv.reader(csv_file)
+    for row in reader:
+      date = datetime.strptime(row[0], '%m.%d.%Y')
+      value = float(row[1])
+      tag_list = [ s.strip() for s in row[2].split(',') ]
+      expense = [date, value, tag_list]
+      expenses.append(expense)
+
+  return expenses
 
 def load_tags(path):
   '''
--- ext/shell/real_data_v1/lib/test_lib.py
+++ ext/shell/real_data_v1/lib/test_lib.py
@@ -2,7 +2,10 @@
 from unittest import TestCase
 import os
 import pdb
-from lib import load_tags, prepare_tags, get_flat_tags
+
+from datetime import datetime
+from IPython.display import HTML
+from lib import load_tags, prepare_tags, get_flat_tags, load_expenses, attach_expenses2tags, calculate_tags_capacity, display_tree
 
 class LibCase(TestCase):
   @classmethod
@@ -15,10 +18,11 @@ class LibCase(TestCase):
   def tearDown(self):
     pass
 
-  def test_lib(self):
+  def test_process_1(self):
     data_folder = os.path.join(os.path.dirname(__file__), 'data_test')
     tags_path = os.path.join(data_folder, 'tags.json')
-    
+    expenses_path = os.path.join(data_folder, 'expenses.csv')
+
     loaded_tags = load_tags(tags_path)
     assert loaded_tags['Tag_2']['_']['Tag_2_3']['_']['Tag_2_3_2'] != None
     
@@ -30,4 +34,32 @@ class LibCase(TestCase):
     assert flat_tags.has_key('Tag_1')
     assert flat_tags.has_key('Tag_2_3_2')
 
-    assert 1 == 1
+    expenses = load_expenses(expenses_path)
+    row = expenses[4]
+    assert type(row[0]) is datetime
+    assert type(row[1]) is float
+    assert type(row[2]) is list
+
+    attach_expenses2tags(expenses, tags)
+    #tag = tags['Tag_2']['_']['Tag_2_3']
+    tag = tags['Tag_1']
+    assert tag['volume'] > 0
+    assert len(tag['expenses']) > 0
+
+    calculate_tags_capacity(tags)
+    tag = tags['Tag_1']
+    assert tag['capacity'] > 0
+
+    tags_html = display_tree(tags)
+    assert type(tags_html) is HTML
+
+  def test_process_2(self):
+    data_folder = os.path.join(os.path.dirname(__file__), 'data_test')
+    tags_path = os.path.join(data_folder, 'tags.json')
+    expenses_path = os.path.join(data_folder, 'expenses.csv')
+    loaded_tags = load_tags(tags_path)
+    tags = prepare_tags(loaded_tags)
+    flat_tags = get_flat_tags(tags)
+    expenses = load_expenses(expenses_path)
+    attach_expenses2tags(expenses, tags)
+    calculate_tags_capacity(tags)
